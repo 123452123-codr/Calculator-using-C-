55dea540b686a05e09296341b44c02aa0eaeb65f
Hackr <81916900+123452123-codr@users.noreply.github.com>
2021-11-01 10:46:21 +0530

    Update Offline.cpp

--- Offline.cpp
+++ Offline.cpp
@@ -2,4 +2,15 @@
 #include<conio.h>
 void main()
 {
+    char op;
+    float a,b;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the operator:";
+    cin>>op;
+    cout<<"Enter the second number:";
+    cin>>b;
+    switch(op)
+    {
+        case 
 }
